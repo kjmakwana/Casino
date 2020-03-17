bd232b6ec8cd117bbd3e8599b32115458b07737f
anush <anushshand11@gmail.com>
2020-03-17 12:55:52 +0530

    Program to find combinations added

--- result.py
+++ result.py
@@ -1,7 +1,12 @@
 #return 1 for royal flush and 10 for high card
-cards=["07H","03H","04H","05D","06C","11S","12S"] #cards dealt to one player + the board
+cards=["07H","03H","03H","05D","06C","11S","12S"] #cards dealt to one player + the board
 hand=["04H","02H","06H","05D","14C"] #5cards selected from the cards list
 from collections import defaultdict as dd
+from itertools import combinations
+
+
+
+
 def check_straight(hand):
     values=[i[0:2] for i in hand]
     values=list(map(int,values))
@@ -10,7 +15,7 @@ def check_straight(hand):
         return 6
     for i in range(values[0], values[0]+5):
         if(i!=values[i-values[0]]):
-            return -1
+            return check_triplet(hand)
     return 6
 
 def check_triplet(hand):
@@ -20,7 +25,7 @@ def check_triplet(hand):
     for i in range(3):
         if(values[i]==values[i+1] and values[i+1]==values[i+2]):
             return 7
-    return -1
+    return check_twopair(hand)
 
 def check_twopair(hand):
     values=[i[0:2] for i in hand]
@@ -31,7 +36,7 @@ def check_twopair(hand):
     if(sorted(values_dict.values())==[1,2,2]):
         return 8
     else:
-        return -1
+        return check_pair(hand)
 
 def check_pair(hand):
     values=[i[0:2] for i in hand]
@@ -42,12 +47,22 @@ def check_pair(hand):
     if(set(values_dict.values())=={1,2}):
         return 9
     else:
-        return -1
+        return 10
+
+def find_rank(cards):
+    pos_hand=list(combinations(cards,5))
+    best_rank=10
+    for hand in pos_hand:
+        rank=check_straight(hand)
+        if(rank<best_rank):
+            best_rank=rank
+    print(best_rank)
+find_rank(cards)
+
 
 
          
 
-print(check_pair(hand))
 
 
 
