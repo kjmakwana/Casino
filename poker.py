a05afe21f7608a58b81a08fa94796755bce472b0
Kshitij Makwana <51596655+kjmakwana@users.noreply.github.com>
2020-03-19 17:15:07 +0530

    Delete poker.py

--- poker.py
+++
@@ -1,7 +0,0 @@
-#First input number of players
-#Subtract intial blinds from the required players
-#Deal number of cards
-#Star the round and see if player calls or bets or checks
-#Open the flop then the river then the turn 
-#Display result
-
